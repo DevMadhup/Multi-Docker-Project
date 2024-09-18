86b6a0c392037818e8c605d2c0c4858ac30dad01
Madhup Pandey <121779953+DevMadhup@users.noreply.github.com>
2024-09-18 15:37:34 +0530

    Create HomeController.cs

---
+++ DOTNET/App/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
